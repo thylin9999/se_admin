cf04ba030b109d30168e2b31ecbe896779fcf298
drawin <andylou9527@gmail.com>
2024-03-19 11:43:02 +0800

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,30 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-  </main>
+    <div class="common-layout">
+        <el-container style="width: 100%;height: 100%">
+            <el-aside width="140px">
+                left
+            </el-aside>
+            <el-container>
+                <el-header height="4%">
+                    top
+                </el-header>
+                <el-main>
+                    mid
+                </el-main>
+                <el-footer height="4%">
+                    Fot
+                </el-footer>
+            </el-container>
+        </el-container>
+    </div>
 </template>
+
+<style scoped>
+.common-layout {
+    width: 100%;
+    height: 100%;
+}
+</style>
